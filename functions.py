ad7ea91c3e9cb5bbe1e12968d7a5f6aa7cab5f02
Khensani Chauke <khensanichauke@Khensanis-MacBook-Air.local>
2026-06-20 00:40:24 +0200

    second commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+#Learning about functions
+
+name = input("What is your name: ")
+surname = input("What is your surname: ")
+
+
+def greet(name, surname):
+    print(f"Hello {name} {surname}, we welcome you to our school of thought. My name is Prinslo. How can I help you today?")
+
+
+greet(name, surname)
+
